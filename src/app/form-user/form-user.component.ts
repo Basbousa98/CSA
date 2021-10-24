b7acd94a8093a9860b023127c6d350a6424f8b8e
Basbousa98 <emna.besbes@esprit.tn>
2021-10-24 10:05:56 +0100

    update

---
+++ src/app/form-user/form-user.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from "../model/user";
+import {UserService} from "../services/user.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-form-user',
+  templateUrl: './form-user.component.html',
+  styleUrls: ['./form-user.component.css']
+})
+export class FormUserComponent implements OnInit {
+  user: User;
+  constructor(private userService: UserService, private route: Router) { }
+
+  ngOnInit(): void {
+    this.user= new User();
+  }
+
+  save(){
+    this.user.accountCategory="Customer";
+    this.userService.list.push(this.user);
+    console.log(this.userService.list)
+    this.route.navigate(['/user'])
+  }
+
+}
